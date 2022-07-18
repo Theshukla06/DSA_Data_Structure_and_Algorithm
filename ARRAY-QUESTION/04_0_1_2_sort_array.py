9a9d5ad8a221d3f786f84199ca89cc97d610eb80
Theshukla06 <en21ca501023@gmail.com>
2022-07-18 13:15:48 +0530

    CHANGE code

--- ARRAY-QUESTION/04_0_1_2_sort_array.py
+++ ARRAY-QUESTION/04_0_1_2_sort_array.py
@@ -5,13 +5,13 @@
 #                         left   current     Right
 #                          LO       Mid        HI
 def SortArray( a, arr1):
-    lo = 0
+    low = 0
     hi = arr1 - 1
     mid = 0
     while mid < hi:
         if a[mid] == 0:
-            a[lo], a[mid] = a[mid], a[lo]
-            lo = lo + 1
+            a[low], a[mid] = a[mid], a[low]
+            low = low + 1
             mid = mid + 1
         elif a[mid] == 1:
             mid = mid + 1
